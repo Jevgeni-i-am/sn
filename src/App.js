3962563727b3f0805aadc3cf390c3887f3c891da
Jevgeni-i-am <jevgeni.liski@gmail.com>
2020-11-02 18:57:48 +0200

    social network first upload

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ let AppContainer = compose(
 
 let SamuraiApp = (props) => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Provider store={store}>
                 <AppContainer/>
             </Provider>
