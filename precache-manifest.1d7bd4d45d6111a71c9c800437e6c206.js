4c8de297db812b1acb92af0175df8c60a929ddee
Jevgeni-i-am <jevgeni.liski@gmail.com>
2020-11-02 19:08:29 +0200

    Updates

---
+++ precache-manifest.1d7bd4d45d6111a71c9c800437e6c206.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc175f994bc74248c1bab28366c27a7",
+    "url": "/sn/index.html"
+  },
+  {
+    "revision": "4435562015c8b26c2ea1",
+    "url": "/sn/static/css/4.b2a08a5b.chunk.css"
+  },
+  {
+    "revision": "d5204008dd35eb5fa581",
+    "url": "/sn/static/css/5.96bae0d7.chunk.css"
+  },
+  {
+    "revision": "1cf0bfefb0dd788d755e",
+    "url": "/sn/static/css/6.6c245bc4.chunk.css"
+  },
+  {
+    "revision": "3239b4cfc9ea5de3b316",
+    "url": "/sn/static/css/7.c6789104.chunk.css"
+  },
+  {
+    "revision": "b246fb1aa056ed15d6ee",
+    "url": "/sn/static/css/8.fd8003c7.chunk.css"
+  },
+  {
+    "revision": "c4afa47443feda55e08c",
+    "url": "/sn/static/css/main.8eb79e0a.chunk.css"
+  },
+  {
+    "revision": "08cd9617de42e7bce12c",
+    "url": "/sn/static/js/0.47bc74c8.chunk.js"
+  },
+  {
+    "revision": "54808f4fd9c9fd654fee",
+    "url": "/sn/static/js/3.3647bc01.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/sn/static/js/3.3647bc01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4435562015c8b26c2ea1",
+    "url": "/sn/static/js/4.2a358673.chunk.js"
+  },
+  {
+    "revision": "d5204008dd35eb5fa581",
+    "url": "/sn/static/js/5.104577ce.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/sn/static/js/5.104577ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf0bfefb0dd788d755e",
+    "url": "/sn/static/js/6.feb1ec2e.chunk.js"
+  },
+  {
+    "revision": "3239b4cfc9ea5de3b316",
+    "url": "/sn/static/js/7.e60dd720.chunk.js"
+  },
+  {
+    "revision": "b246fb1aa056ed15d6ee",
+    "url": "/sn/static/js/8.5fcdf03e.chunk.js"
+  },
+  {
+    "revision": "c4afa47443feda55e08c",
+    "url": "/sn/static/js/main.bfb6dbb2.chunk.js"
+  },
+  {
+    "revision": "45c46fa753932cb25095",
+    "url": "/sn/static/js/runtime-main.04824fab.js"
+  },
+  {
+    "revision": "1a53c75355ca07d639467977d34a27d7",
+    "url": "/sn/static/media/Spinner-1s-200px.1a53c753.svg"
+  },
+  {
+    "revision": "d4bc48c0a4585fd70ae4492dc1a00557",
+    "url": "/sn/static/media/beachparty.d4bc48c0.jpg"
+  }
+]);
--- src/utils/object-helper.js
+++
@@ -1,9 +0,0 @@
-export const updateObjectInArray = (items, itemId, objPropName, newObjectProps) => {
-
-   return items.map(u => {
-        if (u[objPropName]=== itemId) {
-            return {...u, ...newObjectProps}
-        }
-        return u;
-    })
-}
